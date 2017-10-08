2ed170e33c344a553bccbd0c017ccf39890a7101
Thomas Christensen <31951107+Thom9521@users.noreply.github.com>
2017-10-08 18:23:36 +0200

    Små justeringer

--- src/thom9521/GuessANumber.java
+++ src/thom9521/GuessANumber.java
@@ -10,33 +10,33 @@ public class GuessANumber {
             int number = (int)(Math.random() * 101);
 
             Scanner input = new Scanner(System.in);
-            System.out.println("Guess a magic number between 0 and 100");
+            System.out.println("Gæt et magisk nummer mellem 0 og 100");
 
             // Prompt the user to guess the number
-            System.out.print("\nEnter your guess: ");
+            System.out.print("\nIndtast dit gæt: ");
             int guess = input.nextInt();
 
             if (guess == number)
-                System.out.println("Yes, the number is " + number);
+                System.out.println("Ja, nummeret er " + number + " :)");
             else if (guess > number)
-                System.out.println("Your guess is too high");
+                System.out.println("Dit gæt er for højt :(");
             else
-                System.out.println("Your guess is too low");
+                System.out.println("Dit gæt er for lavt :(");
 
 
 
             //Loop som bliver ved med at spørge om et nyt tal ind til det bliver det rigtige.
             while ( guess != number) {
                 System.out.println("");
-                System.out.println("Gæt igen");
+                System.out.println("Gæt igen:");
 
                 guess = input.nextInt();
                 if (guess == number)
-                    System.out.println("Yes, the number is " + number);
+                    System.out.println("Ja, nummeret er " + number + " :)");
                 else if (guess > number)
-                    System.out.println("Your guess is too high");
+                    System.out.println("Dit gæt er for højt :(");
                 else
-                    System.out.println("Your guess is too low");
+                    System.out.println("Dit gæt er for lavt :(");
 
             }
         }
